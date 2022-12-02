17033c4cc138a27bb482c73b040e610de91f67f4
Botond A. Nas <nas.botond@gmail.com>
2022-12-02 19:50:42 +0100

    refactor code

--- include/project-polaris/app.hpp
+++ include/project-polaris/app.hpp
@@ -1,12 +1,5 @@
 #pragma once
 
-#include <stdio.h>
-#include <cstdlib>
-#include <iostream>
-#include <string>
-#include <array>
-#include <thread>
-
 // OpenGL Loader
 #include <GL/gl3w.h> // GL3w, initialized with gl3wInit() below
 
@@ -17,34 +10,14 @@
 #include "imgui.h"
 #include "imgui_impl_glfw.h"
 #include "imgui_impl_opengl3.h"
-#include "vtk_viewer.hpp"
 
-#include <vtkSmartPointer.h>
-#include <vtkActor.h>
-#include <vtkTransform.h>
-#include <vtkTransformPolyDataFilter.h>
-#include <vtkAxesActor.h>
+#include "gui.hpp"
 
-#include "madgwick_filter.hpp"
-#include "comp_filter.hpp"
-#include "csv_reader.hpp"
-#include "metrics.hpp"
-
-#include "vtk_actor_generator.hpp"
-#include "tinyfiledialogs.h"
-
-namespace GUI 
+namespace GUI
 {
 	void setup();
 	void begin();
     void render();
-    void initUI();
-    void generateUI();
     void destroy();
     static void glfw_error_callback(int error, const char* description);
-    static std::string _labelPrefix(const char* const label);
-    void* runFilter(const int &freq, const float &comp_gain, const float &madg_beta, int &start_index, int &end_index, const bool &max_index, std::vector<std::vector<double>> &gravity_vectors_madg_mag,
-    std::vector<std::vector<double>> &gravity_vectors_madg, std::vector<std::vector<double>> &gravity_vectors_gt, std::vector<std::vector<double>> &gravity_vectors_comp_mag, std::vector<std::vector<double>> &gravity_vectors_comp);
-	void* call_from_thread();
-	GLFWwindow* getCurrentWindow();
 }
---
+++ include/project-polaris/gui.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <stdio.h>
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <thread>
+
+#include "vtk_viewer.hpp"
+
+#include <vtkSmartPointer.h>
+#include <vtkActor.h>
+#include <vtkTransform.h>
+#include <vtkTransformPolyDataFilter.h>
+#include <vtkAxesActor.h>
+
+#include "madgwick_filter.hpp"
+#include "comp_filter.hpp"
+#include "csv_reader.hpp"
+#include "metrics.hpp"
+
+#include "vtk_actor_generator.hpp"
+#include "tinyfiledialogs.h"
+
+namespace GUI
+{
+    void initUI();
+    void generateUI();
+    static std::string _labelPrefix(const char* const label);
+	void* runFilter();
+    void* call_from_thread();
+}
